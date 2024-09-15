a921aa416888b6958fab723445f7a01f2e401778
Elena17_77 <agata.miron77@gmail.com>
2024-09-15 22:27:17 +0300

    add diplom

--- diplom/test/test_api.py
+++ diplom/test/test_api.py
@@ -23,7 +23,7 @@ def test_rating_req():
 def test_future_req():
     
     headers = {"X-API-KEY": "G22PTDH-MM6MGEQ-JJTXGTG-ZCZQBQB"}
-    resp = requests.get('https://api.kinopoisk.dev/v1.4/movie?year=2034', headers=headers)
+    resp = requests.get('https://api.kinopoisk.dev/v1.4/movie?id=81-24', headers=headers)
     assert resp.status_code == 400
 
 def test_wrong_rating_req():
--- diplom/test/test_ui.py
+++ diplom/test/test_ui.py
@@ -1,24 +1,35 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+#from selenium.webdriver.chrome.service import Service
+#from webdriver_manager.chrome import ChromeDriverManager
+
+def test_kinopoisk():
+  chrome = webdriver.Chrome()
+  #service=Service(ChromeDriverManager().install())
 
 
-chrome = webdriver.Chrome()
+  chrome.get("https://www.kinopoisk.ru/")
+  #позитивные тесты
+  input_field = chrome.find_element(By.CSS_SELECTOR, 'input[placeholder = "Фильмы, сериалы, персоны"]')
+  input_field.send_keys('шрек')
+  input_field.clear()
 
-chrome.get("https://www.kinopoisk.ru/")
+  chrome.find_element(By.CSS_SELECTOR, 'input[placeholder = "Фильмы, сериалы, персоны"]')
+  input_field.send_keys('А')
+  input_field.clear()
 
-add_button = chrome.find_element(By.XPATH, '//input[@placeholder="Фильмы, сериалы, персоны"]'). click()
+  chrome.find_element(By.CSS_SELECTOR, 'input[placeholder = "Фильмы, сериалы, персоны"]')
+  input_field.send_keys('2')
+  input_field.clear()
 
+#негативные тесты
+  chrome.find_element(By.CSS_SELECTOR, 'input[placeholder = "Фильмы, сериалы, персоны"]')
+  input_field.send_keys(' ')
+  input_field.clear()
 
-add_button = chrome.find_element(By.XPATH, '//button[@aria-label = "Меню"]'). click()
+  chrome.find_element(By.CSS_SELECTOR, 'input[placeholder = "Фильмы, сериалы, персоны"]')
+  input_field.send_keys('-')
+  input_field.clear()
 
-
-add_button = chrome.find_element(By.XPATH, '//a[@href = "/promo"]'). click()
-
-
-add_button = chrome.find_element(By.XPATH, '//a[@href = "/games"]'). click()
-
-
-add_button = chrome.find_element(By.XPATH, '//a[@class = "styles_root__o_aAP styles_rootActive__xFaoQ"]'). click()
-
-chrome.switch_to.alert.accept()
+  chrome.quit()
